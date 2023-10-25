9d78892c3ab89fa3029c585a87c77acec29f4b81
playingwiththegucciflipflopson <135711422+playingwiththegucciflipflopson@users.noreply.github.com>
2023-10-25 12:48:53 +0200

    Add files via upload

---
+++ data_slams.py
@@ -0,0 +1,35 @@
+australian_open = {"2000": "Agassi", "2001": "Agassi", "2002": "Johannson",
+                   "2003": "Agassi", "2004": "Federer", "2005": "Safin",
+                   "2006": "Federer", "2007": "Federer", "2008": "Djokovic",
+                   "2009": "Nadal", "2010": "Federer", "2011": "Djokovic",
+                   "2012": "Djokovic", "2013": "Djokovic", "2014": "Wawrinka",
+                   "2015": "Djokovic", "2016": "Djokovic", "2017": "Federer",
+                   "2018": "Federer", "2019": "Djokovic", "2020": "Djokovic",
+                   "2021": "Djokovic", "2022": "Nadal", "2023": "Djokovic"}
+
+french_open = {"2000": "Kürten", "2001": "Kürten", "2002": "Costa",
+               "2003": "Ferrero", "2004": "Gaudio", "2005": "Nadal",
+               "2006": "Nadal", "2007": "Nadal", "2008": "Nadal",
+               "2009": "Federer", "2010": "Nadal", "2011": "Nadal",
+               "2012": "Nadal", "2013": "Nadal", "2014": "Nadal",
+               "2015": "Wawrinka", "2016": "Djokovic", "2017": "Nadal",
+               "2018": "Nadal", "2019": "Nadal", "2020": "Nadal",
+               "2021": "Djokovic", "2022": "Nadal", "2023": "Djokovic"}
+
+wimbledon = {"2000": "Sampras", "2001": "Ivanisevic", "2002": "Hewitt",
+             "2003": "Federer", "2004": "Federer", "2005": "Federer",
+             "2006": "Federer", "2007": "Federer", "2008": "Nadal",
+             "2009": "Federer", "2010": "Nadal", "2011": "Djokovic",
+             "2012": "Federer", "2013": "Murray", "2014": "Djokovic", 
+             "2015": "Djokovic", "2016": "Murray", "2017": "Federer",
+             "2018": "Djokovic", "2019": "Djokovic", "2020": "-", 
+             "2021": "Djokovic", "2022": "Djokovic", "2023": "Alcaraz"}
+
+us_open = {"2000": "Safin", "2001": "Hewitt", "2002": "Sampras",
+           "2003": "Roddick", "2004": "Federer", "2005": "Federer",
+           "2006": "Federer", "2007": "Federer", "2008": "Federer", 
+           "2009": "Del Potro", "2010": "Nadal", "2011": "Djokovic", 
+           "2012": "Murray", "2013": "Nadal", "2014": "Cilic",
+           "2015": "Djokovic", "2016": "Wawrinka", "2017": "Nadal", 
+           "2018": "Djokovic", "2019": "Nadal", "2020": "Thiem",
+           "2021": "Medvedev", "2022": "Alcaraz", "2023": "Djokovic"}
