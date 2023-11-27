ba89cf182c2fea272d458528a321c9286a72a748
playingwiththegucciflipflopson <135711422+playingwiththegucciflipflopson@users.noreply.github.com>
2023-11-27 15:43:22 +0100

    Add files via upload

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,12 @@
-from src import main
+from src.tennis_functions import *
+from prettytable import PrettyTable
 
-...
+
+def test_main():
+    table = create_tennis_grand_slam_table()
+    assert isinstance(table, PrettyTable)  # sicherstellen, dass Ausgabe im Dateityp Prettytable ist
+    assert table.field_names == ["Jahr", "Australian Open", "French Open", "Wimbledon", "US Open"]  # Überprüfung Überschriften
+
+    assert anzahl_slams_abfrage("Federer") == 20
+    assert anzahl_slams_abfrage("Djokovic") == 20
+    assert anzahl_slams_abfrage("Invalid_Player") == 0
